e87078577838bc8d2808ee849ec474c7eb630739
Sheen <118210155+sheen123atti@users.noreply.github.com>
2024-07-28 00:08:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+//import dotenv from "dotenv";
+//dotenv.config();
+
+//import express from "express";
+const express = require("express");
+const router = express.Router();
+//import cors from "cors";
+const cors = require("cors");
+//import nodemailer from "nodemailer";
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+// console.log(process.env.RECEIVER_EMAIL);
+// console.log(process.env.RECEIVER_PASS);
+const user = process.env.RECEIVER_EMAIL;
+const pass = process.env.RECEIVER_PASS;
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: user,
+    pass: pass
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: user,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
